70bfc81660deaba0c9228b0c42db8e650a3f4ca7
YuriKatalkin <katalkin18@gmail.com>
2025-10-13 13:08:34 +0300

    Homework #3

--- src/lesson3_operators_homework/MyOperators.java
+++ src/lesson3_operators_homework/MyOperators.java
@@ -4,8 +4,19 @@ import java.util.Scanner;
 
 public class MyOperators {
     public static void main(String[] args) {
-        homeworkTaskOne();
+        //homeworkTaskOne();
+        //homeworkTaskTwo();
+        //homeworkTaskThree();
+        //homeworkTaskFour();
+
+        //!!! Не завершенное !!!
+        //taskWithStar();
     }
+
+//    Задача 1:
+//    Напишите программу, которая будет принимать на вход число из консоли и на выход
+//    будет выводить сообщение четное число или нет. Для определения четности числа
+//    используйте операцию получения остатка от деления (операция выглядит так: '% 2').
 
     public static void homeworkTaskOne() {
         Scanner input = new Scanner(System.in);
@@ -19,26 +30,39 @@ public class MyOperators {
         homeworkTaskOne();
     }
 
+    //Задача 2:
+    //Для введенного числа t (температура на улице) вывести: Если t>–5, то вывести «Warm».
+    //Если –5>= t > –20, то вывести «Normal». Если –20>= t, то вывести «Cold».
+
     public static void homeworkTaskTwo() {
         Scanner input = new Scanner(System.in);
-        System.out.println();
         int t = input.nextInt();
         if (t > -5) {
-            System.out.println();
+            System.out.println("Warm");
         } else if (-5 >= t && t >= -20) {
-            System.out.println();
+            System.out.println("Normal");
         } else if (-20 > t) {
-            System.out.println();
+            System.out.println("Cold");
         }
         homeworkTaskTwo();
     }
 
+    //Задача 3:
+    //Составьте программу, выводящую на экран квадраты чисел от 10 до 20 включительно.
+
     public static void homeworkTaskThree() {
-        for (double i = 10; i < 20 ; i++) {
-            System.out.println(Math.pow(i, 2));
+        for (int i = 10; i <= 20 ; i++) {
+            double square = Math.pow(i, 2);
+            System.out.println("Квадрат числа - " + i + " равен - " + square);
+            if (i == 20) {
+                break;
+            }
         }
-        homeworkTaskThree();
     }
+
+    //Задача 4:
+    //Необходимо, чтоб программа выводила на экран вот такую последовательность:
+    //7 14 21 28 35 42 49 56 63 70 77 84 91 98. В решении используйте цикл while.
 
     public static void homeworkTaskFour() {
         int a = 7;
@@ -46,8 +70,8 @@ public class MyOperators {
             a = a + 7;
             System.out.print(a + "...");
         }
-        homeworkTaskFour();
     }
+
     //Задача *:
     //Напишите программу, где пользователь вводит любое целое положительное число. А
     //программа суммирует все числа от 1 до введенного пользователем числа. Для ввода
@@ -57,16 +81,12 @@ public class MyOperators {
        Scanner input = new Scanner(System.in);
        int a = input.nextInt();
 
-
-        int sum = 0;
+       int sum = 0;
         for (int i = 0; i <= a; i++) {
             sum += i;
         }
         System.out.println(sum);
         taskWithStar();
-
-        //
-         //
     }
 }
 
